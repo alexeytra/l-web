bcae9caaffe7cd76fe32a79a10cd407ff8946012
Alex <alekceytr@mail.ru>
2021-06-12 18:00:40 +0800

    init register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,36 @@
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Form, Formik } from 'formik';
 import React from 'react';
+import { Wrapper } from '../components/Wrapper';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  return <div>regiter page</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Логин</FormLabel>
+              <Input
+                value={values.username}
+                onChange={handleChange}
+                id="username"
+                placeholder="логин"
+              />
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
