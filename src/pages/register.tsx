387e1b534955eadd2acf60826b9274c14b5a5e0e
Alex <alekceytr@mail.ru>
2021-06-13 12:37:13 +0800

    improve register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,33 +6,23 @@ import React from 'react';
 import { useMutation } from 'urql';
 import { InputField } from '../components/InputField';
 import { Wrapper } from '../components/Wrapper';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-mutation Register($username: String!, $password: String!){
-  register(options:{username: $username, password: $password}) {
-    errors{
-      field
-      message
-    }
-    user{
-      id
-      username
-    }
-  }
-}
-`
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUTATION);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values);
-          return register(values)
+        onSubmit={async (values, {setErrors}) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors({
+              username: 'HEy im '
+            })
+          }
         }}
       >
         {({ isSubmitting }) => (
